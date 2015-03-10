39fd07a45ede4903b10b5fd13aa1f94e479c99b7
Ivan Miranda <ivan.i.n@hotmail.com>
2015-03-10 17:54:43 -0300

    Adição de rotas e organização de submodulos

--- src/no-reload-core.js
+++ src/no-reload-core.js
@@ -1,12 +1,17 @@
 var NoReload = (function($) {
     var serverAddress = '';
-    var initialState = 'home';
-    var currentState = initialState;
+    var initialRoute = 'home';
+    var lastRoute = initialRoute;
 
-    var controllers = {};
-    var navigationStates = {};
     var preLoadEvents = {};
     var posLoadEvents = {};
+    
+    // The main path matching regexp utility.
+    var PATH_REGEXP = new RegExp([
+        '(\\\\.)',
+        '([\\/.])?(?:\\:(\\w+)(?:\\(((?:\\\\.|[^)])*)\\))?|\\(((?:\\\\.|[^)])*)\\))([+*?])?',
+        '([.+*?=^!:${}()[\\]|\\/])'
+    ].join('|'), 'g');
 
     var utils = {
         defaultValue: function(original, dft) {
@@ -26,15 +31,6 @@ var NoReload = (function($) {
         }
     };
 
-    var formatUrl = function(location) {
-        var loc = location.split('/');
-        var formatedLocation = 'c=' + loc[0];
-        if (loc.length > 1) {
-            formatedLocation += '&m=' + loc[1];
-        }
-        return serverAddress + '?' + formatedLocation;
-    };
-
     var preLoad = function() {
         for (var key in preLoadEvents) {
             preLoadEvents[key]();
@@ -45,59 +41,180 @@ var NoReload = (function($) {
             posLoadEvents[key]();
         }
     };
-
-    var defaultErrorFunction = function() {
-        alert("error");
-    };
-
-    var defaultResponseProcessor = function() {
-        return true;
-    };
-
-    var ajax = function(options) {
-        var defaultOptions = {
-            url: serverAddress,
-            cache: false,
-            contentType: "application/json",
-            dataType: "json",
-            beforeSend: function() {
-                preLoad();
-            },
-            complete: function() {
-                posLoad();
-            },
-            error: function(params) {
-                defaultErrorFunction(params);
+    
+    var ajax = {
+        formatUrl: function(location) {
+            var loc = location.split('/');
+            var formatedLocation = 'c=' + loc[0];
+            if (loc.length > 1) {
+                formatedLocation += '&m=' + loc[1];
             }
-        };
-
-        $.ajax(utils.objectMerge(defaultOptions, options));
+            return serverAddress + '?' + formatedLocation;
+        },
+        defaultErrorFunction: function() {
+            throw "Ajax Error";
+        },
+        run: function(method, url, success) {
+            url = this.formatUrl(url);
+            $.ajax({
+                type: method,
+                url: url,
+                success: success
+            });
+        }
     };
 
-    return {
-        utils: utils,
+    $.ajaxSetup({
+        url: serverAddress,
+        cache: false,
+        contentType: "application/json",
+        dataType: "json",
+        global: false,
+        beforeSend: function() {
+            preLoad();
+        },
+        complete: function() {
+            posLoad();
+        },
+        error: function(params) {
+            ajax.defaultErrorFunction(params);
+        }
+    });
+
+    var controllers = {
+        registredControllers: {},
+        registerController: function(name, controller){
+            this.registredControllers[name] = controller;
+        },
+        defaultResponseProcessor: function() {
+            return true;
+        },
+        call: function(controllerFunc, params){
+            if (typeof controllerFunc === 'string')
+                controllerFunc = this.getControllerFunc(controllerFunc);
+
+            controllerFunc(params);
+        },
+        getControllerFunc: function(name){
+            return function(params) {
+                var names = name.split(';');
+                for (var key in names) {
+                    name = names[key].split('.');
+                    var controllerName = name[0];
+                    var funcName = name[1];
+
+                    if (typeof this.registredControllers[controllerName] !== 'undefined' && 
+                            typeof this.registredControllers[controllerName][funcName] !== 'undefined') {
+                        this.registredControllers[controllerName][funcName](params);
+                    }
+                }
+            };
+        }
+    };
+
+    var routes = {
+        registredRoutes: {},
+        registerRoute: function(route, controllers, isAjax){
+            isAjax = utils.defaultValue(isAjax, true);
+            var routeReg = this.pathtoRegexp(route);
+            registredRoutes[route] = {
+                regExp: routeReg.regExp,
+                routeParams: routeReg.keys,
+                controllers: controllers,
+                isAjax: isAjax
+            };
+        },
+        find: function(route){
+            for(var route in this.registredRoutes){
+                var regExp = this.registredRoutes[route].regExp;
+                if(regExp.test(route)){
+                    return {
+                        definition: this.registredRoutes[route],
+                        matches: route.match(regExp)
+                    };
+                }
+            }
+            return false;
+        },
+        escapeGroup: function(group) {
+            return group.replace(/([=!:$\/()])/g, '\\$1');
+        },
+        pathtoRegexp: function(path) {
+            var keys = [];
+            var index = 0;
+            
+            // Alter the path string into a usable regexp.
+            path = path.replace(PATH_REGEXP, function(match, escaped, prefix, key, capture, group, suffix, escape) {
+                // Avoiding re-escaping escaped characters.
+                if (escaped) {
+                    return escaped;
+                }
+
+                // Escape regexp special characters.
+                if (escape) {
+                    return '\\' + escape;
+                }
+
+                var repeat = suffix === '+' || suffix === '*';
+                var optional = suffix === '?' || suffix === '*';
+    
+                keys.push({
+                  name:      key || index++,
+                  delimiter: prefix || '/',
+                  optional:  optional,
+                  repeat:    repeat
+                });
+
+                // Escape the prefix character.
+                prefix = prefix ? '\\' + prefix : '';
+
+                // Match using the custom capturing group, or fallback to capturing
+                // everything up to the next slash (or next period if the param was
+                // prefixed with a period).
+                capture = this.escapeGroup(capture || group || '[^' + (prefix || '\\/') + ']+?');
+
+                // Allow parameters to be repeated more than once.
+                if (repeat) {
+                    capture = capture + '(?:' + prefix + capture + ')*';
+                }
+
+                // Allow a parameter to be optional.
+                if (optional) {
+                    return '(?:' + prefix + '(' + capture + '))?';
+                }
+
+                // Basic parameter support.
+                return prefix + '(' + capture + ')';
+            });
+
+            return {
+                regExp: new RegExp('^' + path + '$'),
+                keys: keys
+            };
+        }
+    };
+
+    var __export__ = {
         preLoad: preLoad,
         posLoad: posLoad,
+        utils: utils,
         ajax: ajax,
-        startAnchorNavigation: function(){
+        controllers: controllers,
+        routes: routes,
+        startAnchorNavigation: function() {
             $(window).on('hashchange', function() {
                 var name = location.hash.replace(/^#/, '');
                 this.loadState(name);
             });
         },
-        registerState: function(name, state, isAjax) {
-            isAjax = utils.defaultValue(isAjax, true);
-
-            navigationStates[name] = {
-                state: state,
-                isAjax: isAjax
-            };
+        registerRoute: function(name, controller, isAjax) {
+            routes.registerRoute(name, controller, isAjax);
         },
-        isRegistredState: function(name) {
-            return typeof navigationStates[name] !== 'undefined';
+        isRegistredRoute: function(name) {
+            return routes.find(name) !== false;
         },
         registerController: function(name, controller) {
-            controllers[name] = controller;
+            controllers.registerController(name, controller);
         },
         registerPreLoadEvent: function(name, event) {
             preLoadEvents[name] = event;
@@ -111,35 +228,28 @@ var NoReload = (function($) {
         unregisterPosLoadEvent: function(name) {
             delete preLoadEvents[name];
         },
-        loadState: function(name, param) {
-            if (typeof navigationStates[name] !== 'undefined' && typeof navigationStates[name].state === 'function') {
-                if (navigationStates[name].isAjax && typeof param === 'undefined') {
-                    this.request(name, function(response) {
-                        if (defaultResponseProcessor(response)) {
-                            navigationStates[name].state(response);
-                        }
+        load: function(route, params) {
+            route = routes.find(route);
+            var NR = this;
+            if(route){
+                if(route.isAjax && typeof params === 'undefined'){
+                    ajax.run('get', route, function(response){
+                        NR.safeCallControllers(route.definition.controllers, response);
                     });
                 }
-                else {
-                    if (defaultResponseProcessor(param)) {
-                        navigationStates[name].state(param);
-                    }
+                else{
+                    NR.safeCallControllers(route.definition.controllers, params);
                 }
-                currentState = name;
+                lastRoute = route;
             }
-            else if (typeof navigationStates[initialState] !== 'undefined' && typeof navigationStates[initialState].state === 'function') {
-                this.loadState(initialState);
+            else if (routes.find(initialRoute)) {
+                NR.loadState(initialRoute);
             }
         },
-        request: function(location, callback) {
-            if (typeof callback === 'string')
-                callback = this.getControllerFunc(callback);
-
-            ajax({
-                type: 'get',
-                url: formatUrl(location),
-                success: callback
-            });
+        safeCallControllers: function(controllers, params){
+            if(controllers.defaultResponseProcessor(params)){
+                controllers.call(controllers, params);
+            }
         },
         send: function(options) {
             var defaultOptions = {
@@ -151,53 +261,30 @@ var NoReload = (function($) {
             };
             options = utils.objectMerge(defaultOptions, options);
 
-            var NR = this;
-            ajax({
+            $.ajax({
                 type: options.type,
-                url: formatUrl(options.location),
+                url: ajax.formatUrlformatUrl(options.location),
                 data: options.data,
                 success: function(response) {
                     if (defaultResponseProcessor(response)) {
                         if (options.callback) {
-                            NR.call(options.callback, response);
+                            NR.safeCallControllers(options.callback, response);
                         }
-                        ;
                         if (options.reload) {
-                            NR.loadState(currentState, response);
+                            NR.load(lastRoute, response);
                         }
-                        ;
                     }
                 }
             });
         },
-        call: function(controllerFunc, params) {
-            if (typeof controllerFunc === 'string')
-                controllerFunc = this.getControllerFunc(controllerFunc);
-
-            controllerFunc(params);
-        },
-        getControllerFunc: function(name) {
-            return function(params) {
-                var names = name.split(';');
-                for (var key in names) {
-                    name = names[key].split('.');
-                    var controllerName = name[0];
-                    var funcName = name[1];
-
-                    if (typeof controllers[controllerName] !== 'undefined' && typeof controllers[controllerName][funcName] !== 'undefined') {
-                        controllers[controllerName][funcName](params);
-                    }
-                }
-            };
-        },
-        getCurrentState: function() {
-            return currentState;
+        getCurrentRoute: function() {
+            return lastRoute;
         },
         setDefaultErrorFunction: function(func) {
-            defaultErrorFunction = func;
+            ajax.defaultErrorFunction = func;
         },
         setDefaultResponseProcessor: function(func) {
-            defaultResponseProcessor = func;
+            controllers.defaultResponseProcessor = func;
         },
         getServerAddress: function() {
             return serverAddress;
@@ -205,11 +292,13 @@ var NoReload = (function($) {
         setServerAddress: function(address) {
             serverAddress = address;
         },
-        getInitialState: function() {
-            return initialState;
+        getInitialRoute: function() {
+            return initialRoute;
         },
-        setInitialState: function(state) {
-            initialState = state;
+        setInitialRoute: function(state) {
+            initialRoute = state;
         }
     };
+
+    return __export__;
 })(jQuery);
