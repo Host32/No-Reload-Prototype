8833dc3c292d6373495759fe95af0e3980acb3bf
Ivan Miranda <ivan.i.n@hotmail.com>
2015-04-12 14:59:18 -0300

    routes fix

--- dist/no-reload.js
+++ dist/no-reload.js
@@ -362,11 +362,42 @@
 	            if (registered.hasOwnProperty(key)) {
 	                route = registered[key];
 	                if (route.regExp.test(path)) {
-	                    return Routes.createRouteObject(route, path);
+	                    return routes.createRouteObject(route, path);
 	                }
 	            }
 	        }
 	        return null;
+	    };
+
+
+	    /**
+	     * Create a route description object from a route and a path
+	     * @param   {Object} route      registered Route
+	     * @param   {String} calledPath path called from a `load`
+	     * @returns {Object} route description
+	     */
+	    this.createRouteObject = function (route, calledPath) {
+	        'use strict';
+	        var matches = calledPath.match(route.regExp),
+	            routePath = route.path,
+	            replace,
+	            matchedKey,
+	            key;
+
+	        for (key in route.keys) {
+	            if (route.keys.hasOwnProperty(key)) {
+	                matchedKey = parseInt(key, 10) + 1;
+	                matches[route.keys[key].name] = matches[matchedKey];
+	                replace = matches[matchedKey] || '';
+	                routePath = routePath.replace(":" + route.keys[key].name, replace);
+	            }
+	        }
+
+	        return {
+	            definition: route,
+	            matches: matches,
+	            path: routePath
+	        };
 	    };
 
 	    this.isRegistered = function (path) {
@@ -381,36 +412,6 @@
 	            }
 	        }
 	        return false;
-	    };
-	};
-
-	/**
-	 * Create a route description object from a route and a path
-	 * @param   {Object} route      registered Route
-	 * @param   {String} calledPath path called from a `load`
-	 * @returns {Object} route description
-	 */
-	Routes.prototype.createRouteObject = function (route, calledPath) {
-	    'use strict';
-	    var matches = calledPath.match(route.regExp),
-	        routePath = route.path,
-	        replace,
-	        matchedKey,
-	        key;
-
-	    for (key in route.keys) {
-	        if (route.keys.hasOwnProperty(key)) {
-	            matchedKey = parseInt(key, 10) + 1;
-	            matches[route.keys[key].name] = matches[matchedKey];
-	            replace = matches[matchedKey] || '';
-	            routePath = routePath.replace(":" + route.keys[key].name, replace);
-	        }
-	    }
-
-	    return {
-	        definition: route,
-	        matches: matches,
-	        path: routePath
 	    };
 	};
 
--- src/no-reload/routes.js
+++ src/no-reload/routes.js
@@ -49,11 +49,42 @@ var Routes = function () {
             if (registered.hasOwnProperty(key)) {
                 route = registered[key];
                 if (route.regExp.test(path)) {
-                    return Routes.createRouteObject(route, path);
+                    return routes.createRouteObject(route, path);
                 }
             }
         }
         return null;
+    };
+
+
+    /**
+     * Create a route description object from a route and a path
+     * @param   {Object} route      registered Route
+     * @param   {String} calledPath path called from a `load`
+     * @returns {Object} route description
+     */
+    this.createRouteObject = function (route, calledPath) {
+        'use strict';
+        var matches = calledPath.match(route.regExp),
+            routePath = route.path,
+            replace,
+            matchedKey,
+            key;
+
+        for (key in route.keys) {
+            if (route.keys.hasOwnProperty(key)) {
+                matchedKey = parseInt(key, 10) + 1;
+                matches[route.keys[key].name] = matches[matchedKey];
+                replace = matches[matchedKey] || '';
+                routePath = routePath.replace(":" + route.keys[key].name, replace);
+            }
+        }
+
+        return {
+            definition: route,
+            matches: matches,
+            path: routePath
+        };
     };
 
     this.isRegistered = function (path) {
@@ -68,36 +99,6 @@ var Routes = function () {
             }
         }
         return false;
-    };
-};
-
-/**
- * Create a route description object from a route and a path
- * @param   {Object} route      registered Route
- * @param   {String} calledPath path called from a `load`
- * @returns {Object} route description
- */
-Routes.prototype.createRouteObject = function (route, calledPath) {
-    'use strict';
-    var matches = calledPath.match(route.regExp),
-        routePath = route.path,
-        replace,
-        matchedKey,
-        key;
-
-    for (key in route.keys) {
-        if (route.keys.hasOwnProperty(key)) {
-            matchedKey = parseInt(key, 10) + 1;
-            matches[route.keys[key].name] = matches[matchedKey];
-            replace = matches[matchedKey] || '';
-            routePath = routePath.replace(":" + route.keys[key].name, replace);
-        }
-    }
-
-    return {
-        definition: route,
-        matches: matches,
-        path: routePath
     };
 };
 
